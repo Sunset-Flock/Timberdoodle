fa97eae21157c4d9d00d6d2b64b339c6927fd7f3
MatejSakmary <matej.hydro@gmail.com>
2026-02-15 15:39:43 +0100

    [Volumetric] Moved phase functions into a shared hlsl file

--- src/shader_lib/volumetric.hlsl
+++ src/shader_lib/volumetric.hlsl
@@ -2,88 +2,67 @@
 
 #include "daxa/daxa.inl"
 
-#include "shader_shared/volumetric.inl"
+#if DAXA_LANGUAGE == DAXA_LANGUAGE_CPP
+#include "../shader_shared/shared.inl"
+#elif DAXA_LANGUAGE == DAXA_LANGUAGE_SLANG
+#include "shader_shared/shared.inl"
+#endif
 
-// Froxel volume format:
-// rgb9e5 instatter color froxel volume
-// rgb9e4 accumulated inscatter color
-
-struct VolumetricResult
+// ============================================== PHASE FUNCTIONS ==============================================
+SHARED_FUNCTION float rayleigh_phase(float cos_theta)
 {
-    float3 transmittance;
-    float3 inscattering;
-};
-func volumetric_extinction_inscatter(float3 origin, float3 ray, float t, RaytracingAccelerationStructure tlas, float3 sun_direction) -> VolumetricResult
+    float factor = 3.0f / (16.0f * float(PI));
+    return factor * (1.0f + cos_theta * cos_theta);
+}
+
+SHARED_FUNCTION float henyey_greenstein_phase(float g, float cos_theta)
 {
-    VolumetricResult ret = {};
+#if DAXA_LANGUAGE == DAXA_LANGUAGE_CPP
+    return (1.0f / (4.0f * float(PI))) * ((1.0f - (g * g)) / std::pow((1.0f + (g * g) - (2.0f * g * cos_theta)), 3.0f / 2.0f));
+#elif DAXA_LANGUAGE == DAXA_LANGUAGE_SLANG
+    return (1.0f / (4.0f * PI)) * ((1.0f - (g * g)) / pow((1.0f + (g * g) - (2.0f * g * cos_theta)), 3.0f / 2.0f));
+#endif
+}
 
-    float3 fog_albedo = float3(0.5f, 0.8f, 1.0f) * 0.1f;
-    float fog_density = 0.05;
+// https://research.nvidia.com/labs/rtr/approximate-mie/publications/approximate-mie.pdf
+SHARED_FUNCTION float draine_phase(float alpha, float g, float cos_theta)
+{
+    return henyey_greenstein_phase(g, cos_theta) *
+           ((1.0f + (alpha * cos_theta * cos_theta)) / (1.0f + (alpha * (1.0f / 3.0f) * (1.0f + (2.0f * g * g)))));
+}
 
-    float3 fog_extinction_coef = (1.0f - fog_albedo) * fog_density;
-    float3 fog_scattering_coef = (fog_albedo) * fog_density;
-    float3 transmittance = exp(-(fog_extinction_coef * t));
-    ret.transmittance = transmittance;
+SHARED_FUNCTION float hg_draine_phase(float cos_theta, float diameter)
+{
+    const float g_hg = exp(-(0.0990567f / (diameter - 1.67154f)));
+    const float g_d = exp(-(2.20679f / (diameter + 3.91029f)) - 0.428934f);
+    const float alpha = exp(3.62489f - (8.29288f / (diameter + 5.52825f)));
+    const float w_d = exp(-(0.599085f / (diameter - 0.641583f)) - 0.665888f);
 
-    //ret.transmittance = 1.0f.xxx;
-    ret.inscattering = 0.0f.xxx;
+    return ((1.0f - w_d) * draine_phase(0, g_hg, cos_theta)) + (w_d * draine_phase(alpha, g_d, cos_theta));
+}
 
-    // Shitty Raymarch
-    let march_step_size = 1.5;
-    for (uint i = 0; i < 32; ++i)
+// Creating the atmospheric world of red dead redemption 2 - https://www.youtube.com/watch?v=9-HTvoBi0Iw&t=7100s
+SHARED_FUNCTION float multi_octave_hg(float g, float cos_theta, float extinction, float w_0, float w_1, int octaves)
+{
+    const float base = henyey_greenstein_phase(g, cos_theta) * w_0;
+    const float octaves_weight = (w_1 * extinction) / (octaves - 1);
+
+    float octaves_phase = 0.0f;
+    for(int octave = 1; octave <= octaves; ++octave)
     {
-        let march_t = i * march_step_size + march_step_size * rand();
-        if (march_t >= t) 
-            break;
-
-        float3 ws_sample_pos = origin + ray * march_t;
-
-        float3 inscatter = 1.0f - exp(-(fog_scattering_coef * march_step_size));
-
-        RayQuery<RAY_FLAG_CULL_NON_OPAQUE |
-        RAY_FLAG_SKIP_PROCEDURAL_PRIMITIVES |
-        RAY_FLAG_ACCEPT_FIRST_HIT_AND_END_SEARCH> q;
-
-        const float t_min = 0.01f;
-        const float t_max = 1000.0f;
-
-        RayDesc my_ray = {
-            ws_sample_pos,
-            t_min,
-            sun_direction,
-            t_max,
-        };
-
-        // Set up a trace.  No work is done yet.
-        q.TraceRayInline(
-            tlas,
-            0, // OR'd with flags above
-            0xFFFF,
-            my_ray);
-
-        // Proceed() below is where behind-the-scenes traversal happens,
-        // including the heaviest of any driver inlined code.
-        // In this simplest of scenarios, Proceed() only needs
-        // to be called once rather than a loop:
-        // Based on the template specialization above,
-        // traversal completion is guaranteed.
-        q.Proceed();
-
-
-        bool shadowed = false;
-        // Examine and act on the result of the traversal.
-        // Was a hit committed?
-        if(q.CommittedStatus() == COMMITTED_TRIANGLE_HIT)
-        {
-            shadowed = true;
-        }
-
-
-        if (!shadowed)
-        {
-            ret.inscattering += inscatter * float3(1,1,1) * march_step_size;
-        }
+#if DAXA_LANGUAGE == DAXA_LANGUAGE_CPP
+        octaves_phase += henyey_greenstein_phase(std::powf((2.0f/3.0f), float(octave)) * g, cos_theta);
+#elif DAXA_LANGUAGE == DAXA_LANGUAGE_SLANG
+        octaves_phase += henyey_greenstein_phase(pow((2.0f/3.0f), octave) * g, cos_theta);
+#endif
     }
 
-    return ret;
+    const float combined_octaves = base + octaves_weight * octaves_phase;
+#if DAXA_LANGUAGE == DAXA_LANGUAGE_CPP
+    const float backscatter_clamp = (1.0f / float(PI));// * std::max(0.2f, extinction * 0.1f);
+    return std::max(combined_octaves, backscatter_clamp);
+#elif DAXA_LANGUAGE == DAXA_LANGUAGE_SLANG
+    const float backscatter_clamp = (1.0f / float(PI));// * max(0.2, extinction * 0.1);
+    return max(combined_octaves, backscatter_clamp);
+#endif
 }
